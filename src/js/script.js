28949a029efeee43eb8edb36e649a3457b5e08fb
Aleksandr <enerdil@yandex.ru>
2021-12-22 16:54:08 +0500

    Gulp start

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+let button = document.getElementById('burger');
+
+let navigation = document.getElementById('main_menu');
+
+let close = document.getElementById('close');
+
+let search_button = document.getElementById('search_button');
+
+let search = document.getElementById('search_form');
+
+button.addEventListener('click', function(){ 
+  navigation.classList.toggle('display-block');
+});
+
+search_button.addEventListener('click', function(){ 
+  search.classList.toggle('display-block');
+});
+
+close.addEventListener('click', function(){ 
+    navigation.classList.toggle('display-block');
+  });
