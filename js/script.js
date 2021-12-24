90a3b3a7a11506a431f09d3ac32cf6b62eca6098
Aleksandr <enerdil@yandex.ru>
2021-12-24 11:00:37 +0500

    index menu shadow

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let button = document.getElementById('burger');
 
 let navigation = document.getElementById('main_menu');
 
+let shadow = document.getElementById('shadow');
+
 let close = document.getElementById('close');
 
 let search_button = document.getElementById('search_button');
@@ -10,6 +12,10 @@ let search = document.getElementById('search_form');
 
 button.addEventListener('click', function(){ 
   navigation.classList.toggle('display-block');
+});
+
+button.addEventListener('click', function(){ 
+  shadow.classList.toggle('display-block');
 });
 
 search_button.addEventListener('click', function(){ 
