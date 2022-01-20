7bd22b0b59789f28a0e924c79131b886bfecc9db
Aleksandr <enerdil@yandex.ru>
2022-01-20 16:01:02 +0500

    catalog filter button complete

--- js/script.js
+++ js/script.js
@@ -10,6 +10,14 @@ let search_button = document.getElementById('search_button');
 
 let search = document.getElementById('search_form');
 
+let filter_button = document.getElementById('filter_button');
+
+let filter_menu = document.getElementById('filter_menu');
+
+let size_button = document.getElementById('size_button');
+
+let size_menu = document.getElementById('size_menu');
+
 button.addEventListener('click', function(){ 
   navigation.classList.toggle('display-block');
 });
@@ -22,6 +30,14 @@ search_button.addEventListener('click', function(){
   search.classList.toggle('display-block');
 });
 
+filter_button.addEventListener('click', function(){ 
+  filter_menu.classList.toggle('display-block');
+});
+
+size_button.addEventListener('click', function(){ 
+  size_menu.classList.toggle('display-block');
+});
+
 close.addEventListener('click', function(){ 
-    navigation.classList.toggle('display-block');
-  });
+  navigation.classList.toggle('display-block');
+});
